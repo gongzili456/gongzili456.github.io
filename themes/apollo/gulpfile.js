43d607e13d45e5a983f42791a3bed806f9a9ed00
liuguili <gongzili456@gmail.com>
2016-03-11 18:27:44 +0800

    modified theme

--- themes/apollo/gulpfile.js
+++ themes/apollo/gulpfile.js
@@ -1,7 +1,8 @@
-import gulp from 'gulp';
-import sass from 'gulp-sass';
-import autoprefixer from 'gulp-autoprefixer';
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
+// 一次性编译 Sass 
 gulp.task('sass', function() {
     return gulp.src('./source/scss/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
@@ -9,6 +10,7 @@ gulp.task('sass', function() {
         .pipe(gulp.dest('./source/css'));
 });
 
+// 实时编译
 gulp.task('default', ['sass'], function() {
     gulp.watch('./source/scss/_partial/*.scss', ['sass']);
     gulp.watch('./source/scss/*.scss', ['sass']);
